7325711999b766bbca4854fe72b15a7d22fc9569
Jane <593437446@qq.com>
2019-11-29 09:51:10 +0800

    添加日常 vue 插件合合集

---
+++ Template.vue
@@ -0,0 +1,40 @@
+/**
+* @Author: 李晨光
+* @Date: 2019/7/29
+* @Version: 1.0
+* @Last Modified by: 李晨光
+* @Last Modified time: 2019/7/29
+**/
+<comment>
+    # 组件注释
+</comment>
+<template>
+    <div id="Template">
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Template',
+        components: {},
+        props: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        created() {
+        },
+        mounted() {
+        },
+        watch: {},
+        methods: {},
+        destroyed() {
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
